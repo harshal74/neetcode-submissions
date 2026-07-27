23fd9e46838d8a3c4aa5b177621261bdd960da0a
harshal patil <114437191+harshal74@users.noreply.github.com>
2026-07-27 23:07:25 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        // for(int i=0; i<nums.length-1; i++){
+        //     for(int j=i+1; j<nums.length; j++){
+        //         if(nums[i]==nums[j])return true;
+        //     }
+        // }
+        // return false;
+
+        Hashtable <Integer, Integer> map = new Hashtable<>();
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey(nums[i])){
+                return true;
+            }else{
+                map.put(nums[i],1);
+            }
+        }
+            return false;
+    }
+}
